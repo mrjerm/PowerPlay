12b0cfe8846c3228ba4c0a3b20dccc6e4e20d686
mrjerm <jkhsu0423@gmail.com>
2022-11-26 16:08:03 -0500

    UPDATED ALL THE AUTONOMOUSUSSIES!!! NEED TO TEST ALL EXCEPT RED2LOW!!! 🤓🤓🤓🤓🤓

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,77 +10,66 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600, 240);
 
-        RoadRunnerBotEntity Red2Park1 = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity Red2Low = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13)
+                .setConstraints(45, 52.48291908330528, Math.toRadians(273.36816), Math.toRadians(273.36816), 10)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(35, -62, Math.toRadians(90)))
-                                .lineToConstantHeading(new Vector2d(35, -34))
-                                .lineToConstantHeading(new Vector2d(35, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(12, -35))
+                        drive.trajectorySequenceBuilder(new Pose2d(38, -62, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(38.5, -21, Math.toRadians(88)))
+                                .lineToLinearHeading(new Pose2d(37, -12, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(60, -14, Math.toRadians(84)))
+                                .lineToLinearHeading(new Pose2d(51.5, -14, Math.toRadians(86)))
+                                .lineToLinearHeading(new Pose2d(60, -14, Math.toRadians(84)))
+                                .lineToLinearHeading(new Pose2d(51.5, -14, Math.toRadians(86)))
+                                .build()
+                );
+        RoadRunnerBotEntity Red1Low = new DefaultBotBuilder(meepMeep)
+                .setConstraints(45, 52.48291908330528, Math.toRadians(273.36816), Math.toRadians(273.36816), 10)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-38, -62, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(-38.5, -21, Math.toRadians(92)))
+                                .lineToLinearHeading(new Pose2d(-37, -12, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(-60, -14, Math.toRadians(96)))
+                                .lineToLinearHeading(new Pose2d(-51.5, -14, Math.toRadians(94)))
+                                .lineToLinearHeading(new Pose2d(-60, -14, Math.toRadians(96)))
+                                .lineToLinearHeading(new Pose2d(-51.5, -14, Math.toRadians(94)))
+                                .build()
+                );
+        RoadRunnerBotEntity Blue2Low = new DefaultBotBuilder(meepMeep)
+                .setConstraints(45, 52.48291908330528, Math.toRadians(273.36816), Math.toRadians(273.36816), 10)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-38, 62, Math.toRadians(-90)))
+                                .lineToLinearHeading(new Pose2d(-38.5, 21, Math.toRadians(-92)))
+                                .lineToLinearHeading(new Pose2d(-37, 12, Math.toRadians(-90)))
+                                .lineToLinearHeading(new Pose2d(-60, 14, Math.toRadians(-96)))
+                                .lineToLinearHeading(new Pose2d(-51.5, 14, Math.toRadians(-94)))
+                                .lineToLinearHeading(new Pose2d(-60, 14, Math.toRadians(-96)))
+                                .lineToLinearHeading(new Pose2d(-51.5, 14, Math.toRadians(-94)))
+                                .build()
+                );
+        RoadRunnerBotEntity Blue1Low = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(45, 52.48291908330528, Math.toRadians(273.36816), Math.toRadians(273.36816), 10)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(38, 62, Math.toRadians(-90)))
+                                .lineToLinearHeading(new Pose2d(38.5, 21, Math.toRadians(-88)))
+                                .lineToLinearHeading(new Pose2d(37, 12, Math.toRadians(-90)))
+                                .lineToLinearHeading(new Pose2d(60, 14, Math.toRadians(-84)))
+                                .lineToLinearHeading(new Pose2d(51.5, 14, Math.toRadians(-86)))
+                                .lineToLinearHeading(new Pose2d(60, 14, Math.toRadians(-84)))
+                                .lineToLinearHeading(new Pose2d(51.5, 14, Math.toRadians(-86)))
                                 .build()
                 );
 
-        RoadRunnerBotEntity Red2Park2 = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(35, -62, Math.toRadians(90)))
-                                .lineToConstantHeading(new Vector2d(35, -34))
-                                .lineToConstantHeading(new Vector2d(35, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(12, -35))
-                                .lineToConstantHeading(new Vector2d(35, -35))
-                                .build()
-                );
 
-        RoadRunnerBotEntity Red2Park3 = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(35, -62, Math.toRadians(90)))
-                                .lineToConstantHeading(new Vector2d(35, -34))
-                                .lineToConstantHeading(new Vector2d(35, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(55, -12))
-                                .lineToConstantHeading(new Vector2d(12, -12))
-                                .lineToConstantHeading(new Vector2d(12, -35))
-                                .lineToConstantHeading(new Vector2d(58, -35))
-                                .build()
-                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(Red2Park1)
-                .addEntity(Red2Park2)
-                .addEntity(Red2Park3)
+                .addEntity(Red2Low)
+                .addEntity(Red1Low)
+                .addEntity(Blue2Low)
+                .addEntity(Blue1Low)
                 .start();
     }
 }
